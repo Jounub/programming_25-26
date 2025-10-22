7e0bf9d7ba92d0760ac7dcf509c144f1077e94a4
Jounub <59767854+Jounub@users.noreply.github.com>
2025-10-22 15:39:29 +0500

    Lr7

---
+++ Lr7/MyIURestDbService/MyIURestDbService/src/test/java/ru/itlearn/MyIURestDbService/MyIuRestDbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itlearn.MyIURestDbService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyIuRestDbServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
